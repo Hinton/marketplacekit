c3c742687f01084d6c394a7e896b1037836b273a
Andreas Coroiu <andreas.coroiu@gmail.com>
2020-04-05 10:57:06 +0200

    Better category seeding.

--- database/seeds/CategoriesTableSeeder.php
+++ database/seeds/CategoriesTableSeeder.php
@@ -22,13 +22,69 @@ class CategoriesTableSeeder extends Seeder
             array (
                 'id' => 1,
                 'parent_id' => 0,
-                'order' => 1,
-                'name' => 'General',
+                'order' => 0,
+                'name' => 'Volunteers',
                 'slug' => '',
                 'hash' => NULL,
                 'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
                 'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
-            )
+            ),
+            1 => 
+            array (
+                'id' => 2,
+                'parent_id' => 1,
+                'order' => 0,
+                'name' => 'Food shopping',
+                'slug' => '',
+                'hash' => NULL,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            ),
+            2 => 
+            array (
+                'id' => 3,
+                'parent_id' => 1,
+                'order' => 1000,
+                'name' => 'Other',
+                'slug' => '',
+                'hash' => NULL,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            ),
+
+            3 => 
+            array (
+                'id' => 4,
+                'parent_id' => 0,
+                'order' => 0,
+                'name' => 'Donations',
+                'slug' => '',
+                'hash' => NULL,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            ),
+            4 => 
+            array (
+                'id' => 5,
+                'parent_id' => 4,
+                'order' => 0,
+                'name' => 'Visors',
+                'slug' => '',
+                'hash' => NULL,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            ),
+            5 => 
+            array (
+                'id' => 6,
+                'parent_id' => 4,
+                'order' => 1000,
+                'name' => 'Other',
+                'slug' => '',
+                'hash' => NULL,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            ),
         ));
         
         
